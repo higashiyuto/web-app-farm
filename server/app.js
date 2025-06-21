9e310602c9d36c086ebafd6cdec1492a7d250320
higashiyuto <higashiyuuto5@gmail.com>
2025-06-21 13:37:38 +0900

    app.jsの変更

--- server/app.js
+++ server/app.js
@@ -25,7 +25,7 @@ app.use(cors());
 // 例: {"name": "Yuto", "age": 30}のようなJSONデータあったとき、これを
 //     サーバ側で「req.body」として扱えるようになる
 app.use(express.json()); 
-app.use(express.static(path.join(__dirname, '../client')));
+app.use(express.static(path.join(__dirname, '../client/pages/login/index.html')));
 app.use('/api/accounts', accountRoutes); //このURLのルートは「accountRoutes」に任せる
 app.use('/api/tweets', tweetRoutes);
 
