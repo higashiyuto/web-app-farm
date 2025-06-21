65c3d626673bdfa72d06a89a8c6c862dcb8970b9
higashiyuto <higashiyuuto5@gmail.com>
2025-06-21 13:04:42 +0900

    listenの変更

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 require('dotenv').config(); //dotenvモジュールを読み込んで、config関数を実行することで「.env」が読み込めるようになる
 
-const app = require('./app'); //サーバ設定用ファイル「app.js」を読み込む
-app.listen(process.env.PORT, '0.0.0.0' ,()=>{
-    console.log(`サーバ起動: http://localhost:${process.env.PORT}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
 });
